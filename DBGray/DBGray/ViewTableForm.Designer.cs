37b484dc13b44f95b8d27f32d0d4d38a353231b0
WilliBG18 <b-gray.1@onu.edu>
2016-04-14 18:01:38 -0400

    Any changes since last commit
    added form 5

--- DBGray/DBGray/ViewTableForm.Designer.cs
+++ DBGray/DBGray/ViewTableForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.TuplesLB = new System.Windows.Forms.ListBox();
+            this.insert_btn = new System.Windows.Forms.Button();
+            this.rfs_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // TuplesLB
@@ -39,11 +41,31 @@
             this.TuplesLB.Size = new System.Drawing.Size(412, 264);
             this.TuplesLB.TabIndex = 0;
             // 
+            // insert_btn
+            // 
+            this.insert_btn.Location = new System.Drawing.Point(325, 290);
+            this.insert_btn.Name = "insert_btn";
+            this.insert_btn.Size = new System.Drawing.Size(75, 23);
+            this.insert_btn.TabIndex = 1;
+            this.insert_btn.Text = "Insert Tuple";
+            this.insert_btn.UseVisualStyleBackColor = true;
+            // 
+            // rfs_btn
+            // 
+            this.rfs_btn.Location = new System.Drawing.Point(12, 290);
+            this.rfs_btn.Name = "rfs_btn";
+            this.rfs_btn.Size = new System.Drawing.Size(75, 23);
+            this.rfs_btn.TabIndex = 2;
+            this.rfs_btn.Text = "Refresh";
+            this.rfs_btn.UseVisualStyleBackColor = true;
+            // 
             // ViewTableForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(412, 325);
+            this.Controls.Add(this.rfs_btn);
+            this.Controls.Add(this.insert_btn);
             this.Controls.Add(this.TuplesLB);
             this.Name = "ViewTableForm";
             this.Text = "Form4";
@@ -54,5 +76,7 @@
         #endregion
 
         private System.Windows.Forms.ListBox TuplesLB;
+        private System.Windows.Forms.Button insert_btn;
+        private System.Windows.Forms.Button rfs_btn;
     }
 }
